6d36014206de7ec6ff79229499f2b37e0ff018f2
sohwijung <sohwi.j@gmail.com>
2020-07-16 21:39:55 +1000

    Styling edits.

---
+++ interactivity.js
@@ -0,0 +1,21 @@
+const blocks = document.querySelectorAll('.each_question');
+const buttons = document.querySelectorAll('.expand');
+const listOfArrays = [];
+
+blocks.forEach((block) => {
+    listOfArrays.push(Array.from(block.children));
+});
+
+buttons.forEach((button, index) => {
+    button.addEventListener(('click'), e => {
+        const question = listOfArrays[index][1];
+        question.classList.toggle('hidden');
+    })
+})
+
+// buttons.forEach((button) => {
+//     button.addEventListener('click', e => {
+//         console.log(e);
+//         // button.classList.remove('hidden');
+//     });
+// });
